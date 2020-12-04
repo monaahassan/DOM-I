1a485f255de4cc7c50f8d71a4016ae0d07e4ddf1
Mona Hassan <monamia@Monas-MacBook-Pro.local>
2020-12-04 00:14:52 -0600

    Final Project Submission

--- js/index.js
+++ js/index.js
@@ -36,7 +36,104 @@ const siteContent = {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
-
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.src = ("img/logo.png");
+
+let ctaImg = document.getElementById("cta-img");
+ctaImg.setAttribute('src', siteContent["cta"]["img-src"])
+ctaImg.src = ("img/header-img.png");
+
+let middleImg = document.getElementById("middle-img");
+middleImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+middleImg.src = ("img/mid-page-accent.jpg");
+
+// ### Task 2a: MVP
+
+// #### Create selectors to point your data into elements
+// * [x] Create selectors by using any of the DOM element's methods
+
+//NAV
+
+const nav = document.querySelectorAll('nav a');
+nav[0].innerText = "Services";
+
+const navOne = document.querySelectorAll('nav a');
+nav[1].innerText = "Product";
+
+const navTwo = document.querySelectorAll('nav a');
+nav[2].innerText = "Vision";
+
+const navThree = document.querySelectorAll('nav a');
+nav[3].innerText = "Features";
+
+const navFour = document.querySelectorAll('nav a');
+nav[4].innerText = "About";
+
+const navFive = document.querySelectorAll('nav a');
+nav[5].innerText = "Contact";
+
+//Title
+let title = document.querySelector(".cta h1");
+title.innerText = "Dom Is Awesome";
+
+
+
+//Button 
+const button = document.querySelector("button");
+button.innerText = "Get Started";
+
+
+//Text Content
+const info = document.querySelectorAll('.text-content h4');
+info[0].textContent = siteContent['main-content']['features-h4'];
+info[1].innerText = siteContent['main-content']['about-h4'];
+info[2].innerText = siteContent['main-content']['services-h4'];
+info[3].innerText = siteContent['main-content']['product-h4'];
+info[4].innerText = siteContent['main-content']['vision-h4'];
+
+
+//Info Text
+
+const infoText = document.querySelectorAll('.text-content p');
+infoText[0].textContent = siteContent['main-content']['features-content'];
+infoText[1].innerText = siteContent['main-content']['about-content'];
+infoText[2].innerText = siteContent['main-content']['services-content'];
+infoText[3].innerText = siteContent['main-content']['product-content'];
+infoText[4].innerText = siteContent['main-content']['vision-content'];
+
+
+//Contact 
+let contactTop = document.querySelector(".contact h4");
+contactTop.innerText = "Contact";
+
+
+
+let contactText = document.querySelectorAll('.contact p');contactText[0].innerText = siteContent['contact']['address'];
+contactText[1].innerText = siteContent['contact']['phone'];
+contactText[2].innerText = siteContent['contact']['email'];
+
+//Footer
+let foot = document.querySelector("footer p");
+foot.innerText = "Copyright Great Idea! 2018";
+
+
+
+
+// * [ ] Note that IDs have been used on all images. Use the IDs to update src path content
+
+
+
+
+// #### Update the HTML with the JSON data
+
+// * [ ] Remember, NO direct updating of the HTML source is allowed.
+// * [ ] Using your selectors, update the content to match the example file.
+// * [ ] Remember to update the src attributes on images
+
+// #### Add new content
+
+// * [ ] Change the color of the navigation text to be green.
+// * [ ] Utilize `.appendChild()` and `.prepend()` to add two new items to the navigation system. You can call them whatever you want.
+// * [ ] Check your work by looking at the [original html](original.html) in the browser
